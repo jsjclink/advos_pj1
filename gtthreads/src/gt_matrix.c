1445a3bc5368436f4a96b32a2d6e693001c8c701
jsjclink <jsjclink@naver.com>
2024-02-12 17:04:03 -0500

    modified matrix

--- gtthreads/src/gt_matrix.c
+++ gtthreads/src/gt_matrix.c
@@ -154,7 +154,9 @@ int main()
 				uarg->mat_size = mat_sizes[msi];
 				uarg->tid = inx;
 				uarg->gid = 0;
-				uarg->assigned_credit = credit_groups[cdi];
+				
+				uarg->assigned_credit = 100000000;
+				// uarg->assigned_credit = credit_groups[cdi];
 
 				uthread_create(&utids[inx], uthread_mulmat, uarg, uarg->gid, uarg->assigned_credit);
 			}
