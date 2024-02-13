1d640c1553e0242828469766beca831d8b1d6bed
jsjclink <jsjclink@naver.com>
2024-02-12 21:10:29 -0500

    yield

--- gtthreads/src/gt_matrix.c
+++ gtthreads/src/gt_matrix.c
@@ -78,6 +78,9 @@ static void * uthread_mulmat(void *p)
 			for(int k = 0; k < ptr->mat_size; k++){
 				C.m[i][j] += (A.m[i][k]) * (B.m[k][j]);
 
+				if(ptr->tid % 3 == 0){
+					uthread_yield();
+				}
 				// if(ptr->tid == 79 && i == 16 && j == 16 && k == 16){
 				// 	fprintf(stderr, "\n[YIELD CALLED] thread_id: %d", ptr->tid);
 				// 	uthread_yield();
